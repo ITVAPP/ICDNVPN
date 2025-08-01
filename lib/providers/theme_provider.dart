5365ae213bf175fb2bd5b0a9429a10cf4559e60d
电视宝 www.itvapp.net <service@itvapp.net>
2025-08-01 11:36:58 +0800

    Create theme_provider.dart

---
+++ lib/providers/theme_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  static const String _themeKey = 'theme_mode';
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  ThemeProvider() {
+    _loadTheme();
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeModeIndex = prefs.getInt(_themeKey) ?? ThemeMode.system.index;
+    _themeMode = ThemeMode.values[themeModeIndex];
+    notifyListeners();
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(_themeKey, mode.index);
+    notifyListeners();
+  }
+}
